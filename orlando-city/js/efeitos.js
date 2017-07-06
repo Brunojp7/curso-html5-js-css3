7a103c9911cecbac19cc9bd0cdf87e493b51f910
BrunoJ <brnjparpagnoli@gmail.com>
2017-07-06 12:51:18 -0300

    News responsive

--- orlando-city/js/efeitos.js
+++ orlando-city/js/efeitos.js
@@ -21,6 +21,21 @@
 
 				});
 
-				$(".owl-carousel").owlCarousel();
+				$('.owl-carousel').owlCarousel({
+				    loop:true,
+				    margin:10,
+				    nav:true,
+				    responsive:{
+				        0:{
+				            items:1
+				        },
+				        600:{
+				            items:3
+				        },
+				        1000:{
+				            items:5
+				        }
+				    }
+				})
 
 			});
